783849962b7ef3946c9349f0775fc7c729af0d97
ringular <ritesh.angural@gmail.com>
2015-09-21 22:13:56 +0800

    update cookbook path

--- solo.rb
+++ solo.rb
@@ -1,3 +1,3 @@
 file_cache_path   "/var/chef-solo/cache"
-cookbook_path     [ "/Users/Ritesh/Development/chef-mautic/cookbooks" ]
+cookbook_path     [ "/Users/Ritesh/Development/chef-mautic" ]
 data_bag_path     [ "#{current_dir}/data_bags" ]
